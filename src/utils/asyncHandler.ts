4438a4e291461a76926b17d24ad2d2e0e2735583
bayger <patryk.bajer@gmail.com>
2026-03-07 18:57:48 +0100

    refactor: remove logging of async request handling in asyncHandler

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -6,7 +6,6 @@ import logger from './logger';
  */
 export function asyncHandler(fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) {
   return (req: Request, res: Response, next: NextFunction) => {
-    logger.info({ method: req.method, url: req.url, body: req.body }, 'Handling async request');
     Promise.resolve(fn(req, res, next)).catch(next);
   };
 }
